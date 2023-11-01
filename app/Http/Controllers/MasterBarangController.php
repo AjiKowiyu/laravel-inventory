4d136332a34a605875f3bf014f1b2bf9bd370d66
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-11-01 20:54:17 +0700

    master barang: perbaikan create

--- app/Http/Controllers/MasterBarangController.php
+++ app/Http/Controllers/MasterBarangController.php
@@ -28,7 +28,7 @@ class MasterBarangController extends Controller
         $aturan = [
             'html_kode' => 'required|min:3|max:7|alpha_dash',
             'html_nama' => 'required|min:10|max:25',
-            'html_deskripsi' => 'max:255',
+            'html_deskripsi' => 'required|max:255',
         ];
         $pesan_indo = [
             'required' => 'Wajib diisi bos!!',
@@ -45,7 +45,7 @@ class MasterBarangController extends Controller
                 //jika inputan user sesuai
                 //simpan ke database
                 $insert = MasterBarangModel::create([
-                    'kode'              => $request->html_kode,
+                    'kode'              => strtoupper($request->html_kode),
                     'nama'              => $request->html_nama,
                     'deskripsi'         => $request->html_deskripsi,
                     'id_kategori'       => null,
