b82fc7c78c9f74bde39e4af4e66f8579e2c0550f
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-11-03 21:13:01 +0700

    master barang detail: perkondisian isset

--- resources/views/master/barang/detail.blade.php
+++ resources/views/master/barang/detail.blade.php
@@ -2,25 +2,29 @@
 @section('master-konten')
 
 <h1>Detail barang</h1>
+@if (isset($barang[0]))
+    @php
+        //konversi tanggal format sql menjadi gampang dibaca oleh manusia
+        $tanggal_dibuat = new DateTime($barang[0]->dibuat_kapan);
+        $dibuat = $tanggal_dibuat->format('D, d M Y');
 
-@php
-    //konversi tanggal format sql menjadi gampang dibaca oleh manusia
-    $tanggal_dibuat = new DateTime($barang[0]->dibuat_kapan);
-    $dibuat = $tanggal_dibuat->format('D, d M Y');
+        //konversi tanggal format sql menjadi gampang dibaca oleh manusia
+        $tanggal_diperbarui = new DateTime($barang[0]->diperbarui_kapan);
+        $diperbarui = $tanggal_diperbarui->format('D, d M Y');
+    @endphp
 
-    //konversi tanggal format sql menjadi gampang dibaca oleh manusia
-    $tanggal_diperbarui = new DateTime($barang[0]->diperbarui_kapan);
-    $diperbarui = $tanggal_diperbarui->format('D, d M Y');
-@endphp
-
-<div class="card w-50 shadow">
-    <div class="card-body">
-        <h5 class="card-title">{{ $barang[0]->kode }}</h5>
-        <h6 class="card-title">{{ $barang[0]->nama }}</h6>
-        <p class="card-text">{{ $barang[0]->deskripsi }}</p>
-        <span class="card-text">Dibuat: {{ $dibuat }} | {{ $barang[0]->dibuat_nama }}</span><br>
-        <span class="card-text">Terakhir Diperbarui: {{ $diperbarui }} | {{ $barang[0]->diperbarui_nama }}</span>
+    <div class="card w-50 shadow">
+        <div class="card-body">
+            <h5 class="card-title">{{ $barang[0]->kode }}</h5>
+            <h6 class="card-title">{{ $barang[0]->nama }}</h6>
+            <p class="card-text">{{ $barang[0]->deskripsi }}</p>
+            <span class="card-text">Dibuat: {{ $dibuat }} | {{ $barang[0]->dibuat_nama }}</span><br>
+            <span class="card-text">Terakhir Diperbarui: {{ $diperbarui }} | {{ $barang[0]->diperbarui_nama }}</span>
+        </div>
     </div>
-</div>
+@else
+    <h2>tidak ada data</h2>
+@endif
+
 
 @endsection
